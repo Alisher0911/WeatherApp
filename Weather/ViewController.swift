278dd002efa0c16a08b52d3b0120ba0269137bf8
Alisher0911 <alisher.pvl67@gmail.com>
2021-02-10 15:08:58 +0600

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Alisher on 2/10/21.
+//  Copyright © 2021 Alisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
